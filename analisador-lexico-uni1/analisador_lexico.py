21d7b5f5c5ef30864c22d7995f74a509ce7cc447
eduardopvieira <eduardoopazvieira@gmail.com>
2024-12-05 08:24:20 -0300

    mudancas

--- analisador-lexico-uni1/analisador_lexico.py
+++ analisador-lexico-uni1/analisador_lexico.py
@@ -1,40 +1,41 @@
 import re
 
-#tokens registrados:
-
 TOKENS = [
 
     ("NOME_INDIVIDUO", r"([A-Z][a-z]+)+[0-9]"), # Eduardo1, MikaelJohnatan2
+    ("PALAVRA_RESERVADA", r"([A-Z][a-z]+)+:"), #EquivalentTo: e palavras com :' 
     ("CLASSE", r"([A-Z][a-z]+[_]?)+"), # Pizza, Pizza_Margherita, PizzaMargherita, Pizza_Margherita_
     ("PROPRIEDADE", r"has([A-Z][a-z]+)+|is([A-Z][a-z]+)+Of|[a-z]+"), # hasAbcDe, isAbcDeOf, abc
-    ("TIPO_DE_DADO", r"[a-z]+: [a-z]+"), #owl: algo
-    ("PALAVRA_RESERVADA", r"[A-Z]+"), #SOME, OF, THAT, ABC
-    ("CARACTERE_ESPECIAL", r"[ \[ \] { } ( ) , < > ]"), #[ , ], (), ><
+    ("TIPO_DE_DADO", r"[a-z]+: ?[a-z]+"), #owl: algo
     ("ESPACO_BRANCO", r"\s"),
+    ("CARACTERE_ESPECIAL", r"[ \[ \] { } ( ) , < > ]"), #[ , ], (), ><
 
 ]
 
-#ignore abaixo, apenas teste
 
-def lexer(input_code):
+def lexer(input):
     tokens = []
-    while input_code:
+    while input:
         match = None
-        for token_name, token_regex in TOKENS:
-            pattern = re.compile(token_regex)
-            match = pattern.match(input_code)
+        for token_nome, token_regex in TOKENS:
+            padrao = re.compile(token_regex)
+            match = padrao.match(input)
             if match:
-                lexeme = match.group(0)
-                if token_name != "ESPACO_BRANCO":  # ignorando espaços em branco
-                    tokens.append((token_name, lexeme))
-                input_code = input_code[len(lexeme):]  # vai avançando o codigo
+                lexema = match.group(0)
+                if token_nome != "ESPACO_BRANCO":  # ignorando espaços em branco
+                    tokens.append((token_nome, lexema))
+                input = input[len(lexema):]  # vai avançando o codigo
                 break
         if not match:
-            # CASO NAO DE NENHUM MATCH VEM PRA CA E DA ERRO
-            raise SyntaxError(f"Token inválido: '{input_code[0]}' em '{input_code}'")
+            
+            print("deu ruim")
+            #raise SyntaxError(f"Token inválido: '{input_code[0]}' em '{input_code}'")
+            break
     return tokens
 
-# Exemplo de uso
-code = "ALO"
+# USAR:
+#arquivo = open('codigo.txt')
+#codigo = arquivo.read
+code = "EquivalentTo: Person and (purchasedPizza some Pizza) and"
 result = lexer(code)
 print(result)

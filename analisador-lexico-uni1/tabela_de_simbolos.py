d0bd0de4ed66dd07928c36872c834d31462778df
Eduardo <eduardopazvieiraa@gmail.com>
2024-12-05 17:17:44 -0300

    classe da tabela de simbolos

---
+++ analisador-lexico-uni1/tabela_de_simbolos.py
@@ -0,0 +1,29 @@
+class TabelaDeSimbolos:
+    def __init__(self, prev=None):
+
+        self.tabela = {}  # <-- DICIONARIO ONDE É GUARDADO O IDENTIFICADOR E O TIPO
+        self.prev = prev  # <-- PONTEIRO PARA A TABELA ANTERIOR (nenhuma no momento)
+
+    def adicionar_simbolo(self, identificador, tipo):
+        self.tabela[identificador] = tipo
+
+    def buscar_tipo(self, identificador):
+        if identificador in self.tabela:
+            return self.tabela[identificador]
+        elif self.prev:  # Se não encontrar, busca na tabela anterior
+            return self.prev.buscar_tipo(identificador)
+        return None
+
+    def printarTabela(self):
+        print("Tabela de Símbolos Atual:")
+        if not self.tabela:
+            print("  <Vazio>")
+        else:
+            for identificador, tipo in self.tabela.items():
+                print(f" [\'{identificador}\' : \'{tipo}\']")
+
+    def registrarResultado(self):
+        with open('resultado_analise.txt', 'w') as arquivo:
+            for identificador, tipo in self.tabela.items():
+                arquivo.write(f" [\'{identificador}\' : \'{tipo}\']\n")
+                print('registrado com sucesso')

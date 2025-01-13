1660cfb7e35faf6b80c496723dc7b210d9f6da04
eduardopvieira <eduardoopazvieira@gmail.com>
2025-01-13 11:44:21 -0300

    alteracoes brabo ta rodando

--- analisador-lexico-uni1/analisadorLexico.py
+++ analisador-lexico-uni1/analisadorLexico.py
@@ -1,20 +1,21 @@
 import ply.lex as lex
 import ply.yacc as yacc
 
-# Classe substituta para TabelaDeSimbolos
-class TabelaDeSimbolos:
-    def __init__(self):
-        self.simbolos = []
+# class TabelaDeSimbolos:
+#     def __init__(self):
+#         self.simbolos = []
 
-    def adicionar_simbolo(self, lexema, tipo):
-        self.simbolos.append((lexema, tipo))
+#     def adicionar_simbolo(self, lexema, tipo):
+#         self.simbolos.append((lexema, tipo))
 
-    def exibir(self):
-        print("Tabela de Símbolos:")
-        for simbolo in self.simbolos:
-            print(simbolo)
+#     def exibir(self):
+#         print("Tabela de Símbolos:")
+#         for simbolo in self.simbolos:
+#             print(simbolo)
 
-# Tokens 
+#!======================================
+#! DEFINIÇÕES DO LEXER
+#!======================================
 tokens = [
     "NOME_INDIVIDUO",
     "PALAVRA_RESERVADA",
@@ -27,16 +28,21 @@ tokens = [
     "CARDINALIDADE",
     "ABRE_PARENT",
     "FECHA_PARENT",
+    "ABRE_CHAVE",
+    "FECHA_CHAVE",
     "EQUIVALENT_TO",
     "OR",
-    "SUBCLASS0F",
+    "AND",
+    "SUBCLASSOF",
     "INDIVIDUALS",
-    "DISJOINTCLASSES"
+    "DISJOINTCLASSES",
+    "SOME",
+    "ONLY"
 ]
 
 # Regras de expressão regular para os tokens
 t_NOME_INDIVIDUO = r'([A-Z][a-z]+)+[0-9]+'
-t_PALAVRA_RESERVADA = r'[Ss][Oo][Mm][Ee]|[Aa][Ll][Ll]|[Vv][Aa][Ll][Uu][Ee]|[Mm][Ii][Nn]|[Ee][Xx][Aa][Cc][Tt][Ll][Yy]|[Tt][Hh][Aa][Tt]|[Mm][Aa][Xx]|[Nn][Oo][Tt]|[Aa][Nn][Dd]|Class:|DisjointWith:|and|some|only'
+t_PALAVRA_RESERVADA = r'[Aa][Ll][Ll]|[Vv][Aa][Ll][Uu][Ee]|[Mm][Ii][Nn]|[Ee][Xx][Aa][Cc][Tt][Ll][Yy]|[Tt][Hh][Aa][Tt]|[Mm][Aa][Xx]|[Nn][Oo][Tt]|Class:|DisjointWith:'
 t_CLASSE = r'([A-Z][a-z]+[_]?)+' 
 t_NAMESPACE = r'[a-z]{3,4}:'
 t_TIPO = r'rational|real|langString|PlainLiteral|XMLLiteral|Literal|anyURI|base64Binary|boolean|byte|dateTime|dateTimeStamp|decimal|double|float|hexBinary|integer|int|language|long|Name|NCName|negativeInteger|NMTOKEN|nonNegativeInteger|nonPositiveInteger|normalizedString|positiveInteger|short|string|token|unsignedByte|unsignedInt|unsignedLong|unsignedShort'
@@ -46,13 +52,17 @@ t_OPERADORES = r'[<>="]{1,2}'
 t_CARDINALIDADE = r'[0-9]+'
 t_ABRE_PARENT = r'\('
 t_FECHA_PARENT = r'\)'
+t_ABRE_CHAVE = r'\{'
+t_FECHA_CHAVE = r'\}'
 t_EQUIVALENT_TO = r'EquivalentTo:'
 t_OR = r'[Oo][Rr]'
+t_AND = r'and'
+t_SOME = r'some'
+t_ONLY = r'only'
 t_SUBCLASSOF = r'SubClassOf:'
 t_INDIVIDUALS = r'Individuals:' 
 t_DISJOINTCLASSES = r'DisjointClasses:'
-# Ignora os espaços em branco e tabulações
-t_ignore = ' \t'
+t_ignore = ' \t' #ESPAÇOS EM BRANCO E TABULAÇOES
 
 def t_newline(t):
     r'\n+'
@@ -65,8 +75,8 @@ def t_error(t):
         log_file.write(erro)
     t.lexer.skip(1)
 
-# Instancia do analisador léxico
-lexer = lex.lex(errorlog=lex.NullLogger())
+# INSTANCIANDO O LEXER
+lexer = lex.lex()
 
 #?======================== REGRAS PRIMÁRIAS ============================= 
 def p_programa(p):
@@ -76,37 +86,41 @@ def p_programa(p):
 
 def p_tipo_classe_primaria(p):
     """
-    tipo_classe_primaria: declaracao_classe_definida
+    tipo_classe_primaria : declaracao_classe_definida
                         | declaracao_classe_primitiva
     """
     pass
 
 def p_tipo_classe_secundaria(p):
     """
-    tipo_classe_secundaria: declaracao_classe_aninhada
+    tipo_classe_secundaria : declaracao_classe_aninhada
                           | declaracao_classe_enumerada
                           | declaracao_classe_coberta
+                          | declaracao_classe_axioma_fechamento
     """
     pass
 
 #!===================== CLASSE PRIMITIVA ================
 
 def p_declaracao_classe_primitiva(p):
-    """declaracao_classe_primitiva : PALAVRA_RESERVADA CLASSE caso_subclassof caso_individuals_opcional caso_disjoint_opcional"""
+    """
+    declaracao_classe_primitiva : PALAVRA_RESERVADA CLASSE caso_subclassof caso_individuals_opcional caso_disjoint_opcional
+    """
     pass
 
 def p_caso_subclassof(p):
     """
-    caso_subclassof: SUBCLASSOF PROPRIEDADE PALAVRA_RESERVADA CLASSE
+    caso_subclassof : SUBCLASSOF PROPRIEDADE PALAVRA_RESERVADA CLASSE
                    | SUBCLASSOF PROPRIEDADE PALAVRA_RESERVADA NAMESPACE TIPO
-                   | SUBCLASSOF PROPRIEDADE PALAVRA_RESERVADA CLASSE CARACTERE_ESPECIAL caso_subclassof 
-                   | SUBCLASSOF PROPRIEDADE PALAVRA_RESERVADA NAMESPACE TIPO CARACTERE_ESPECIAL caso_subclassof
+                   | SUBCLASSOF PROPRIEDADE PALAVRA_RESERVADA CLASSE CARACTERE_ESPECIAL continuacao_subclassof 
+                   | SUBCLASSOF PROPRIEDADE PALAVRA_RESERVADA NAMESPACE TIPO CARACTERE_ESPECIAL continuacao_subclassof
+                   | SUBCLASSOF declaracao_classe_axioma_fechamento
     """
     pass
 
 def p_caso_individuals_opcional(p):
     """
-    caso_individuals_opcional: NOME_INDIVIDUO CARACTERE_ESPECIAL caso_individuals_opcional
+    caso_individuals_opcional : NOME_INDIVIDUO CARACTERE_ESPECIAL caso_individuals_opcional
                              | NOME_INDIVIDUO
                              | INDIVIDUALS NOME_INDIVIDUO
                              | INDIVIDUALS NOME_INDIVIDUO CARACTERE_ESPECIAL caso_individuals_opcional
@@ -115,199 +129,98 @@ def p_caso_individuals_opcional(p):
 
 def p_caso_disjoint_opcional(p):
     """
-    caso_disjoint_opcional: CLASSE CARACTERE_ESPECIAL caso_disjoint_opcional
+    caso_disjoint_opcional : CLASSE CARACTERE_ESPECIAL caso_disjoint_opcional
                          |  CLASSE
                          |  DISJOINTCLASSES CLASSE 
                          |  DISJOINTCLASSES CLASSE CARACTERE_ESPECIAL caso_disjoint_opcional
     """
+    pass
 
-def p_restricoes_primitiva(p):
-    """restricoes_primitiva : CARACTERE_ESPECIAL PALAVRA_RESERVADA CLASSE restricoes
-                  | """
+def p_continuacao_subclassof(p):
+    """continuacao_subclassof : PROPRIEDADE PALAVRA_RESERVADA CLASSE
+                   | PROPRIEDADE PALAVRA_RESERVADA NAMESPACE TIPO
+                   | PROPRIEDADE PALAVRA_RESERVADA CLASSE CARACTERE_ESPECIAL continuacao_subclassof 
+                   | PROPRIEDADE PALAVRA_RESERVADA NAMESPACE TIPO CARACTERE_ESPECIAL continuacao_subclassof
+    """
     pass
 
 #!==================== CLASSE DEFINIDA ==================
 
 def p_declaracao_classe_definida(p):
-    """declaracao_classe_definida : PALAVRA_RESERVADA CLASSE EQUIVALENT_TO CLASSE PALAVRA_RESERVADA restricoes_definida"""
+    """
+    declaracao_classe_definida : PALAVRA_RESERVADA CLASSE EQUIVALENT_TO tipo_classe_secundaria caso_individuals_opcional 
+    """
     pass
 
-# def p_restricoes_definida(p):
-#     """restricoes_definida : ABRE_PARENT PROPRIEDADE PALAVRA_RESERVADA CLASSE FECHA_PARENT
-#                            | ABRE_PARENT PROPRIEDADE PALAVRA_RESERVADA CLASSE FECHA_PARENT PALAVRA_RESERVADA restricoes_definida
-
-#                            | ABRE_PARENT PROPRIEDADE PALAVRA_RESERVADA CARDINALIDADE CLASSE FECHA_PARENT   
-#                            | ABRE_PARENT PROPRIEDADE PALAVRA_RESERVADA CARDINALIDADE CLASSE FECHA_PARENT PALAVRA_RESERVADA  restricoes_definida
-
-#                            | ABRE_PARENT PROPRIEDADE PALAVRA_RESERVADA restricoes_definida FECHA_PARENT 
-#                            | ABRE_PARENT PROPRIEDADE PALAVRA_RESERVADA  caso_definida FECHA_PARENT """
-                           
-#     pass
-
-# def p_caso_definida(p):
-#       """caso_definida : CLASSE 
-#                     | CLASSE PALAVRA_RESERVADA  FECHA_PARENT caso_definida
-#                     | CLASSE PALAVRA_RESERVADA  FECHA_PARENT 
-#                     | ABRE_PARENT CLASSE PALAVRA_RESERVADA caso_definida FECHA_PARENT"""
-# pass
-            
-#!===================== CLASSE AXIOMA DE FECHAMENTO ========================
-
-def p_declaracao_classe_axioma_fechamento(p):
-    """declaracao_classe : PALAVRA_RESERVADA CLASSE PALAVRA_RESERVADA CLASSE CARACTERE_ESPECIAL restricoes_axioma_fechamento """
-    pass
-
-# def p_restricoes_axioma_fechamento(p):
-#     """restricoes_axioma_fechamento : PROPRIEDADE PALAVRA_RESERVADA CLASSE
-#                   | PROPRIEDADE PALAVRA_RESERVADA CLASSE CARACTERE_ESPECIAL restricoes_axioma_fechamento
-#                   | PROPRIEDADE PALAVRA_RESERVADA CARDINALIDADE CLASSE CARACTERE_ESPECIAL restricoes_axioma_fechamento
-#                   | PROPRIEDADE PALAVRA_RESERVADA CARDINALIDADE CLASSE 
-#                   | PROPRIEDADE PROPRIEDADE PALAVRA_RESERVADA CARDINALIDADE CLASSE CARACTERE_ESPECIAL restricoes_axioma_fechamento
-#                   | PROPRIEDADE PROPRIEDADE PALAVRA_RESERVADA CARDINALIDADE CLASSE 
-#                   | caso_axioma 
-#                   | PROPRIEDADE PROPRIEDADE PALAVRA_RESERVADA CLASSE
-#                   | PROPRIEDADE PROPRIEDADE PALAVRA_RESERVADA CLASSE CARACTERE_ESPECIAL restricoes_axioma_fechamento
-#                   | PROPRIEDADE PALAVRA_RESERVADA CARACTERE_ESPECIAL CLASSE PALAVRA_RESERVADA CLASSE  
-#                   | PROPRIEDADE PALAVRA_RESERVADA CARACTERE_ESPECIAL CLASSE PALAVRA_RESERVADA CLASSE CARACTERE_ESPECIAL CARACTERE_ESPECIAL restricoes_axioma_fechamento"""
-#     pass
-                
-
-# def p_caso_axioma(p):
-#     """caso_axioma : ABRE_PARENT PROPRIEDADE PALAVRA_RESERVADA CLASSE FECHA_PARENT
-#                     | ABRE_PARENT PROPRIEDADE PALAVRA_RESERVADA CLASSE FECHA_PARENT CARACTERE_ESPECIAL restricoes_axioma_fechamento                    
-#                     | ABRE_PARENT PROPRIEDADE PALAVRA_RESERVADA CLASSE FECHA_PARENT PALAVRA_RESERVADA caso_axioma"""
-#     pass
-
-
-
-# Declaração de classe aninhada com suporte para múltiplas restrições e conectivos
 def p_declaracao_classe_aninhada(p):
     """
-    declaracao_classe : PALAVRA_RESERVADA CLASSE PALAVRA_RESERVADA CLASSE ands_aninhada FECHA_PARENT"""
-    pass
-
-# Suporte para cláusulas conectadas por "and" dentro de restrições
-def p_ands_aninhada(p):
+    declaracao_classe_aninhada : caso_ands
     """
-    ands_aninhada : PALAVRA_RESERVADA restricoes_aninhada ands_aninhada
-                  | PALAVRA_RESERVADA restricoes_aninhada"""
     pass
 
-# Restrições aninhadas com cardinalidades, agrupamentos e conectivos lógicos
+def p_caso_ands(p):
+    """
+    caso_ands : AND restricoes_aninhada caso_ands
+              | AND restricoes_aninhada
+    """
+    pass
+
 def p_restricoes_aninhada(p):
     """
     restricoes_aninhada : ABRE_PARENT PROPRIEDADE PALAVRA_RESERVADA CLASSE FECHA_PARENT
-                        | ABRE_PARENT PROPRIEDADE PALAVRA_RESERVADA ABRE_PARENT lista_classes FECHA_PARENT
+                        | ABRE_PARENT PROPRIEDADE PALAVRA_RESERVADA ABRE_PARENT classes_and FECHA_PARENT
                         | ABRE_PARENT PROPRIEDADE PALAVRA_RESERVADA CLASSE restricoes_aninhada
                         | ABRE_PARENT PROPRIEDADE PALAVRA_RESERVADA CARDINALIDADE CLASSE FECHA_PARENT restricoes_aninhada
                         | ABRE_PARENT restricoes_aninhada FECHA_PARENT"""
     pass
 
-# Suporte para listas de classes em conectivos lógicos
-def p_lista_classes(p):
+def p_classes_and(p):
     """
-    lista_classes : CLASSE
-                  | CLASSE conectivo_logico lista_classes"""
+    classes_and : CLASSE AND classes_and
+                | CLASSE
+    """
+#!===================== CLASSE AXIOMA DE FECHAMENTO ========================
+
+def p_declaracao_classe_axioma_fechamento(p):
+    """declaracao_classe_axioma_fechamento : CLASSE CARACTERE_ESPECIAL restricoes_axioma_fechamento """
     pass
 
-# Conectivos lógicos para combinações de classes e restrições
-def p_conectivo_logico(p):
+def p_restricoes_axioma_fechamento(p):
     """
-    conectivo_logico : PALAVRA_RESERVADA
-    """
-    pass
-
-# Definição de classes aninhadas e agrupamentos recursivos
-def p_aninhada(p):
-    """
-    aninhada : ABRE_PARENT CLASSE PALAVRA_RESERVADA CLASSE FECHA_PARENT
-             | ABRE_PARENT CLASSE PALAVRA_RESERVADA aninhada FECHA_PARENT
-             | CLASSE PALAVRA_RESERVADA aninhada
-             | CLASSE
+    restricoes_axioma_fechamento : PROPRIEDADE SOME CLASSE
+                                | PROPRIEDADE SOME CLASSE CARACTERE_ESPECIAL restricoes_axioma_fechamento
+                                | PROPRIEDADE ONLY ABRE_PARENT classes_or FECHA_PARENT
     """
     pass
 
-# def p_declaracao_classe_aninhada(p):
-#     """
-#     declaracao_classe : PALAVRA_RESERVADA CLASSE PALAVRA_RESERVADA CLASSE PALAVRA_RESERVADA ABRE_PARENT restricoes_aninhada FECHA_PARENT
-#     """
-#     pass
-
-# def p_restricoes_aninhada(p):
-#     """
-#     restricoes_aninhada : PROPRIEDADE PALAVRA_RESERVADA CLASSE
-#                         | PROPRIEDADE PALAVRA_RESERVADA ABRE_PARENT CLASSE PALAVRA_RESERVADA aninhada
-#                         | ABRE_PARENT PROPRIEDADE PALAVRA_RESERVADA CLASSE restricoes_aninhada
-#                         | PALAVRA_RESERVADA ABRE_PARENT PROPRIEDADE PALAVRA_RESERVADA CARDINALIDADE CLASSE FECHA_PARENT restricoes_aninhada
-#     """
-#     pass
-#                         #| PROPRIEDADE PALAVRA_RESERVADA CLASSE restricoes_aninhada
-
-# def p_aninhada(p):
-#     """
-#     aninhada : ABRE_PARENT CLASSE PALAVRA_RESERVADA CLASSE FECHA_PARENT
-#               | ABRE_PARENT CLASSE PALAVRA_RESERVADA aninhada FECHA_PARENT
-#               | CLASSE PALAVRA_RESERVADA aninhada 
-#               | CLASSE
-#     """
-#     pass
-
-#================================================================
-
-# def p_declaracao_classe_aninhada(p): 
-#      """declaracao_classe_aninhada : PALAVRA_RESERVADA CLASSE PALAVRA_RESERVADA CLASSE restricoes_aninhada"""
-#      pass
-
-
-
-# def p_restricoes_aninhada(p): 
-#     """restricoes_aninhada : PALAVRA_RESERVADA ABRE_PARENT PROPRIEDADE PALAVRA_RESERVADA CLASSE FECHA_PARENT 
-#                             |  PALAVRA_RESERVADA ABRE_PARENT PROPRIEDADE PALAVRA_RESERVADA CLASSE FECHA_PARENT restricoes_aninhada"""
-#     pass
-def p_restricoes_composta(p):
-    """restricoes_composta : restricoes CARACTERE_ESPECIAL PROPRIEDADE PALAVRA_RESERVADA CLASSE
-                           | restricoes CARACTERE_ESPECIAL PALAVRA_RESERVADA CARACTERE_ESPECIAL CLASSE"""
+def p_classes_or(p):
+    """
+    classes_or : CLASSE OR classes_or
+              | CLASSE 
+    """
     pass
 
-def p_disjunto(p):
-    """disjunto : PALAVRA_RESERVADA CLASSE CARACTERE_ESPECIAL CLASSE
-                | """
-    pass
-
-def p_individuos(p):
-    """individuos : PALAVRA_RESERVADA NOME_INDIVIDUO
-                  | individuos CARACTERE_ESPECIAL NOME_INDIVIDUO"""
-    pass
-
-# Classe Definida
-def p_declaracao_classe_definida(p):
-    """declaracao_classe : PALAVRA_RESERVADA CLASSE CARACTERE_ESPECIAL restricoes_def"""
-    pass
-
-def p_restricoes_def(p):
-    """restricoes_def : CLASSE PALAVRA_RESERVADA restricoes
-                      | restricoes_composta CARACTERE_ESPECIAL restricoes"""
-    pass
-
-# Classe Enumerada
+#!======================== CLASSE ENUMERADA ==========================
 def p_declaracao_classe_enumerada(p):
-    """declaracao_classe : PALAVRA_RESERVADA CLASSE CARACTERE_ESPECIAL CARACTERE_ESPECIAL lista_individuos CARACTERE_ESPECIAL"""
+    """
+    declaracao_classe_enumerada : ABRE_CHAVE classes_enumeradas FECHA_CHAVE
+    """
     pass
 
-def p_lista_individuos(p):
-    """lista_individuos : NOME_INDIVIDUO
-                         | lista_individuos CARACTERE_ESPECIAL NOME_INDIVIDUO"""
-    pass
+def p_classes_enumeradas(p):
+    """
+    classes_enumeradas : CLASSE CARACTERE_ESPECIAL classes_enumeradas
+                      | CLASSE
+    """
 
-# Classe Coberta
+#!======================== CLASSE COBERTA ============================
+
 def p_declaracao_classe_coberta(p):
-    """declaracao_classe : PALAVRA_RESERVADA CLASSE CARACTERE_ESPECIAL lista_classes"""
+    """
+    declaracao_classe_coberta : classes_or
+    """
     pass
 
-def p_lista_classes(p):
-    """lista_classes : CLASSE
-                      | lista_classes PALAVRA_RESERVADA CLASSE"""
-    pass
 
 # Erro sintático
 def p_error(p):

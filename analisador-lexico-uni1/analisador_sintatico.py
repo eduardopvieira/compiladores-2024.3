946a467e6a59f75d8943969e490b679c975a043a
mikael-johnatan <mikael.azevedo@alunos.ufersa.edu.br>
2025-01-16 11:25:54 -0300

    Alteração regras sintaticas coop
    Co-authored-by: Eduardo Paz Vieira <eduardopvieira@users.noreply.github.com>

--- analisador-lexico-uni1/analisador_sintatico.py
+++ analisador-lexico-uni1/analisador_sintatico.py
@@ -28,7 +28,8 @@ tokens = [
     "OR",
     "AND",
     "SOME",
-    "ONLY"
+    "ONLY",
+    "VALUE"
 ]
 
 
@@ -76,12 +77,15 @@ def t_DISJOINTWITH(t):
 def t_COMPARADORES(t):
     r'min|exactly|max'
     return t
+def t_VALUE(t):
+    r'value'
+    return t
 
 #!======================== REGEX GENERICOS =====================
 
 t_NOME_INDIVIDUO = r'([A-Z][a-z]+)+[0-9]+'
-t_PALAVRA_RESERVADA = r'[Aa][Ll][Ll]|[Vv][Aa][Ll][Uu][Ee]|[Tt][Hh][Aa][Tt]|[Nn][Oo][Tt]|Class:'
-t_CLASSE = r'([A-Z]+[a-z]+[_]?)+' 
+t_PALAVRA_RESERVADA = r'[Aa][Ll][Ll]|[Tt][Hh][Aa][Tt]|[Nn][Oo][Tt]|Class:'
+t_CLASSE = r'\b([A-Z]+[a-z]+[_]?)+\b' 
 t_TIPO = r'\b(rational|real|langString|PlainLiteral|XMLLiteral|Literal|anyURI|base64Binary|boolean|byte|dateTime|dateTimeStamp|decimal|double|float|hexBinary|integer|int|language|long|Name|NCName|negativeInteger|NMTOKEN|nonNegativeInteger|nonPositiveInteger|normalizedString|positiveInteger|short|string|token|unsignedByte|unsignedInt|unsignedLong|unsignedShort)\b'
 t_PROPRIEDADE = r'has([A-Z][a-z]+)+|is([A-Z][a-z]+)+Of|[a-z]+([A-Z][a-z]+)*' 
 t_CARACTERE_ESPECIAL = r'[\[\].,\"\']'
@@ -137,37 +141,14 @@ def p_tipo_classe_secundaria(p):
 
 def p_declaracao_classe_primitiva(p):
     """
-    declaracao_classe_primitiva : PALAVRA_RESERVADA CLASSE caso_subclassof caso_disjoint_opcional caso_individuals_opcional
-                                | PALAVRA_RESERVADA CLASSE caso_subclassof caso_disjoint_opcional
-                                | PALAVRA_RESERVADA CLASSE caso_subclassof caso_individuals_opcional
-                                | PALAVRA_RESERVADA CLASSE caso_subclassof
+    declaracao_classe_primitiva : PALAVRA_RESERVADA CLASSE SUBCLASSOF continuacao_subclassof caso_disjoint_opcional caso_individuals_opcional
+                                | PALAVRA_RESERVADA CLASSE SUBCLASSOF continuacao_subclassof caso_individuals_opcional caso_disjoint_opcional
+                                | PALAVRA_RESERVADA CLASSE SUBCLASSOF continuacao_subclassof caso_disjoint_opcional
+                                | PALAVRA_RESERVADA CLASSE SUBCLASSOF continuacao_subclassof caso_individuals_opcional
+                                | PALAVRA_RESERVADA CLASSE SUBCLASSOF continuacao_subclassof
     """
-    if len(p) == 6:  # Com caso_subclassof, caso_disjoint_opcional e caso_individuals_opcional
-        p[0] = ("Classe Primitiva ", p[2], p[3], p[4], p[5])
-    elif len(p) == 5:  # Com caso_subclassof e caso_disjoint_opcional
-        p[0] = ("Classe Primitiva ", p[2], p[3], p[4])
-    else:  # Com caso_subclassof e caso_individuals_opcional
-        p[0] = ("Classe Primitiva ", p[2], p[3])
-
-
-def p_caso_subclassof(p):
-    """
-    caso_subclassof : SUBCLASSOF PROPRIEDADE SOME CLASSE
-                   | SUBCLASSOF PROPRIEDADE SOME NAMESPACE TIPO
-                   | SUBCLASSOF PROPRIEDADE SOME CLASSE CARACTERE_ESPECIAL continuacao_subclassof 
-                   | SUBCLASSOF PROPRIEDADE SOME NAMESPACE TIPO CARACTERE_ESPECIAL continuacao_subclassof
-                   | SUBCLASSOF declaracao_classe_axioma_fechamento
-    """
-    if len(p) == 3:
-        p[0] = p[2]
-    elif len(p) == 5:
-        p[0] = (p[2], p[4])
-    elif len(p) == 6:
-        p[0] = (p[2], p[4], p[5])
-    elif len(p) == 7:
-        p[0] = (p[2], p[4], p[5], p[6])
-    else:
-        p[0] = (p[2], p[4], p[5], p[6], p[7])
+    p[0] = (p[2], ": Classe primitiva ")
+        
 
 def p_caso_individuals_opcional(p):
     """
@@ -180,17 +161,6 @@ def p_continuacao_individuals(p):
     continuacao_individuals : NOME_INDIVIDUO CARACTERE_ESPECIAL continuacao_individuals
                             | NOME_INDIVIDUO
     """
-    
-    if len(p) == 2:  # Apenas NOME_INDIVIDUO
-        p[0] = p[1]
-    elif len(p) == 3:  # INDIVIDUALS seguido de NOME_INDIVIDUO
-        p[0] = (p[1], p[2])
-    elif len(p) == 4:  # NOME_INDIVIDUO seguido de CARACTERE_ESPECIAL e caso_individuals_opcional
-        p[0] = (p[1], p[2], p[3])
-    elif len(p) == 5:  # INDIVIDUALS seguido de NOME_INDIVIDUO, CARACTERE_ESPECIAL e caso_individuals_opcional
-        p[0] = (p[1], p[2], p[3], p[4])
-    else:  # Caso vazio (regra sem tokens)
-        p[0] = None
 
 def p_caso_disjoint_opcional(p):
     """
@@ -201,71 +171,58 @@ def p_caso_disjoint_opcional(p):
                          |  DISJOINTWITH CLASSE 
                          |  DISJOINTWITH CLASSE CARACTERE_ESPECIAL caso_disjoint_opcional
     """
-                         #|  empty
-    
-    
-    if len(p) == 2:  # Apenas CLASSE
-        p[0] = p[1]
-    elif len(p) == 3:  # DISJOINTWITH ou DISJOINTCLASSES seguido de CLASSE
-        p[0] = (p[1], p[2])
-    elif len(p) == 4:  # CLASSE seguido de CARACTERE_ESPECIAL e caso_disjoint_opcional
-        p[0] = (p[1], p[2], p[3])
-    elif len(p) == 5:  # DISJOINTWITH ou DISJOINTCLASSES seguido de CLASSE, CARACTERE_ESPECIAL e caso_disjoint_opcional
-        p[0] = (p[1], p[2], p[3], p[4])
-    else:  # Caso vazio
-        p[0] = None
 
 def p_continuacao_subclassof(p):
     """continuacao_subclassof : PROPRIEDADE SOME CLASSE
+                   | CLASSE CARACTERE_ESPECIAL continuacao_subclassof
                    | PROPRIEDADE SOME NAMESPACE TIPO
                    | PROPRIEDADE SOME CLASSE CARACTERE_ESPECIAL continuacao_subclassof 
                    | PROPRIEDADE SOME NAMESPACE TIPO CARACTERE_ESPECIAL continuacao_subclassof
     """
-    if len(p) == 4:  # PROPRIEDADE SOME CLASSE ou NAMESPACE TIPO
-        p[0] = (p[1], p[2], p[3])
-    elif len(p) == 6:  # PROPRIEDADE SOME CLASSE ou NAMESPACE TIPO com CARACTERE_ESPECIAL e continuacao_subclassof
-        p[0] = (p[1], p[2], p[3], p[4], p[5])
 
 #!==================== CLASSE DEFINIDA ==================
 
 def p_declaracao_classe_definida(p):
     """
-    declaracao_classe_definida : PALAVRA_RESERVADA CLASSE EQUIVALENT_TO caso_simples_opcional estrutura_definida
-                               | PALAVRA_RESERVADA CLASSE EQUIVALENT_TO estrutura_definida
+    declaracao_classe_definida : PALAVRA_RESERVADA CLASSE EQUIVALENT_TO continuacao_equivalentto caso_individuals_opcional
+                               | PALAVRA_RESERVADA CLASSE EQUIVALENT_TO continuacao_equivalentto
+                               | PALAVRA_RESERVADA CLASSE EQUIVALENT_TO continuacao_equivalentto SUBCLASSOF continuacao_subclassof
+                               | PALAVRA_RESERVADA CLASSE continuacao_subclassof EQUIVALENT_TO continuacao_equivalentto
     """
-    if len(p) == 5:  # Com estrutura_definida apenas
-        p[0] = ("Classe Definida: ", p[1], p[2], p[3], p[4])
-    elif len(p) == 6:  # Com caso_simples_opcional e estrutura_definida
-        p[0] = ("Classe Definida: ", p[1], p[2], p[3], p[4], p[5])
+    if "SUBCLASSOF" in p.slice and "declaracao_classe_coberta" in p.slice:
+        p[0] = (p[2], "Classe Definida coberta")
+    else:
+        p[0] = (p[2], "Classe Definida")
 
-def p_estrutura_definida(p): 
+
+def p_continuacao_equivalentto(p):
     """
-    estrutura_definida : caso_individuals_opcional estrutura_definida
-                       | tipo_classe_secundaria estrutura_definida
-                       | declaracao_classe_coberta 
+        continuacao_equivalentto : CLASSE AND ABRE_PARENT PROPRIEDADE SOME CLASSE FECHA_PARENT
+                                 | CLASSE AND ABRE_PARENT PROPRIEDADE SOME NAMESPACE TIPO CARACTERE_ESPECIAL OPERADORES CARDINALIDADE CARACTERE_ESPECIAL FECHA_PARENT
+                                 | CLASSE declaracao_classe_aninhada
+                                 | CLASSE OR declaracao_classe_coberta
     """
-    if len(p) == 3:  # caso_individuals_opcional ou tipo_classe_secundaria seguido de estrutura_definida
-        p[0] = (p[1], p[2])
-    elif len(p) == 2:  # Apenas declaracao_classe_coberta
-        p[0] = p[1]
-    else:  # Caso vazio
-        p[0] = None
+
+def p_fechamento(p):
+    """
+    fechamento : ONLY ABRE_PARENT classes_or FECHA_PARENT
+               | ONLY CLASSE
+               | ONLY CLASSE CARACTERE_ESPECIAL
+               | ONLY CLASSE CARACTERE_ESPECIAL PROPRIEDADE COMPARADORES CARDINALIDADE CLASSE
+    """
+    pass
 
 def p_caso_simples_opcional(p):
     """
     caso_simples_opcional : CLASSE caso_ands
     """
-                          #| empty
-    if len(p) == 3:  # CLASSE seguido de caso_ands
-        p[0] = (p[1], p[2])
-    else:  # Caso vazio
-        p[0] = None
+
 
 def p_declaracao_classe_aninhada(p):
     """
     declaracao_classe_aninhada : caso_ands
     """
-    p[0] = p[1]
+    p[0] = (" aninhada ")
 
 def p_caso_ands(p):
     """
@@ -273,48 +230,23 @@ def p_caso_ands(p):
               | AND restricoes_aninhada
               | AND restricoes_aninhada_sem_parentese caso_ands
               | AND restricoes_aninhada_sem_parentese              
-
     """
-    if len(p) == 3:  # AND seguido de restricoes_aninhada ou restricoes_aninhada_sem_parentese
-        p[0] = (p[1], p[2])
-    elif len(p) == 4:  # AND seguido de restricoes_aninhada ou restricoes_aninhada_sem_parentese e caso_ands
-        p[0] = (p[1], p[2], p[3])
+
 
 def p_restricoes_aninhada(p):
     """
-    restricoes_aninhada : ABRE_PARENT PROPRIEDADE SOME CLASSE FECHA_PARENT
-                        | ABRE_PARENT PROPRIEDADE ONLY CLASSE FECHA_PARENT
-                        | ABRE_PARENT PROPRIEDADE ONLY restricoes_aninhada FECHA_PARENT
-                        | ABRE_PARENT PROPRIEDADE PALAVRA_RESERVADA CLASSE FECHA_PARENT
-                        | ABRE_PARENT PROPRIEDADE SOME ABRE_PARENT classes_and FECHA_PARENT
-                        | ABRE_PARENT PROPRIEDADE SOME restricoes_aninhada FECHA_PARENT
-                        | ABRE_PARENT PROPRIEDADE SOME CARDINALIDADE CLASSE FECHA_PARENT restricoes_aninhada
-                        | ABRE_PARENT PROPRIEDADE SOME NAMESPACE TIPO CARACTERE_ESPECIAL OPERADORES CARDINALIDADE CARACTERE_ESPECIAL FECHA_PARENT
-                        | ABRE_PARENT PROPRIEDADE SOME NAMESPACE TIPO CARACTERE_ESPECIAL OPERADORES CARDINALIDADE CARACTERE_ESPECIAL FECHA_PARENT CARACTERE_ESPECIAL restricoes_aninhada
+    restricoes_aninhada : ABRE_PARENT PROPRIEDADE ONLY CLASSE FECHA_PARENT
+                        | ABRE_PARENT PROPRIEDADE ONLY ABRE_PARENT classes_or FECHA_PARENT FECHA_PARENT
                         | ABRE_PARENT PROPRIEDADE COMPARADORES CARDINALIDADE CLASSE FECHA_PARENT
-                        | ABRE_PARENT restricoes_aninhada FECHA_PARENT
-                        | ABRE_PARENT restricoes_aninhada caso_ands FECHA_PARENT
-                        | ABRE_PARENT classes_or FECHA_PARENT"""
-    if len(p) == 6:  # Casos básicos com FECHA_PARENT
-        p[0] = (p[2], p[3], p[4])
-    elif len(p) == 7:  # restricoes_aninhada com ABRE_PARENT ou casos com ONLY/SOME
-        p[0] = (p[2], p[3], p[4], p[5])
-    elif len(p) == 8:  # Casos com restricoes_aninhada e CARDINALIDADE
-        p[0] = (p[2], p[3], p[4], p[5], p[6])
-    elif len(p) == 9:  # NAMESPACE e TIPO com CARACTERE_ESPECIAL
-        p[0] = (p[2], p[3], p[4], p[5], p[6], p[7], p[8])
-    elif len(p) == 10:  # NAMESPACE com TIPO, OPERADORES e CARDINALIDADE
-        p[0] = (p[2], p[3], p[4], p[5], p[6], p[7], p[8], p[9])
-    elif len(p) == 11:  # NAMESPACE e TIPO com operadores extras
-        p[0] = (p[2], p[3], p[4], p[5], p[6], p[7], p[8], p[9], p[10])
-    elif len(p) == 12:  # Casos aninhados com CARACTERE_ESPECIAL e restricoes_aninhada
-        p[0] = (p[2], p[3], p[4], p[5], p[6], p[7], p[8], p[9], p[10], p[11])
-    elif len(p) == 4:  # restricoes_aninhada simples entre parênteses
-        p[0] = (p[2],)
-    elif len(p) == 5:  # restricoes_aninhada com caso_ands
-        p[0] = (p[2], p[3])
-    else:  # Caso com classes_or
-        p[0] = p[2]
+                        | ABRE_PARENT restricoes_aninhada OR restricoes_aninhada FECHA_PARENT
+    """
+
+# Class: RegulatoryActivity
+#     EquivalentTo: ValueActivity
+                #( prop some ())
+#         and ((bundles some (CnAObject or CoreObject or PoPObject)) or 
+#                 (consumes some CounterObject)
+#                 )
 
 def p_restricoes_aninhada_sem_parentese(p):
     """
@@ -324,38 +256,24 @@ def p_restricoes_aninhada_sem_parentese(p):
                                      |  PROPRIEDADE PALAVRA_RESERVADA CLASSE 
                                      |  PROPRIEDADE COMPARADORES CARDINALIDADE CLASSE 
     """
-    if len(p) == 4: 
-        p[0] = (p[1], p[2], p[3])
-    elif len(p) == 5:
-        p[0] = (p[1], p[2], p[3], p[4])
-    else:
-        p[0] = None
 
 def p_classes_and(p):
     """
     classes_and : CLASSE AND classes_and
                 | CLASSE
     """
-    if len(p) == 2:
-        p[0] = [p[1]]
-    elif len(p) == 4:
-        p[0] = [p[1]] + p[3]
 
 
 #!===================== CLASSE AXIOMA DE FECHAMENTO ========================
 
 def p_declaracao_classe_axioma_fechamento(p):
-    """declaracao_classe_axioma_fechamento : CLASSE CARACTERE_ESPECIAL restricoes_axioma_fechamento"""
-    if len(p) == 4:  # CLASSE seguida de CARACTERE_ESPECIAL e restrições
-        p[0] = (p[1], p[2], p[3])
+    """declaracao_classe_axioma_fechamento : CLASSE CARACTERE_ESPECIAL restricoes_axioma_fechamento fechamento """
+    p[0] = (" fechamento ")
 
 def p_restricoes_axioma_fechamento(p):
     """
-    restricoes_axioma_fechamento : casos_propriedade ONLY ABRE_PARENT classes_or FECHA_PARENT
-                                 | casos_propriedade SOME CLASSE CARACTERE_ESPECIAL restricoes_axioma_fechamento
+    restricoes_axioma_fechamento : casos_propriedade SOME CLASSE CARACTERE_ESPECIAL restricoes_axioma_fechamento 
                                  | casos_propriedade SOME CLASSE
-                                 | casos_propriedade ONLY CLASSE 
-                                 | casos_propriedade ONLY CLASSE CARACTERE_ESPECIAL restricoes_axioma_fechamento
                                  | casos_propriedade COMPARADORES CARDINALIDADE CLASSE
                                  | casos_propriedade COMPARADORES CARDINALIDADE CLASSE CARACTERE_ESPECIAL restricoes_axioma_fechamento
                                  | ABRE_PARENT restricoes_axioma_fechamento FECHA_PARENT 
@@ -363,44 +281,27 @@ def p_restricoes_axioma_fechamento(p):
                                  | ABRE_PARENT restricoes_axioma_fechamento FECHA_PARENT CARACTERE_ESPECIAL restricoes_axioma_fechamento
                                  
     """
-    if len(p) == 6 and p[2] == "ONLY" and p[3] == "ABRE_PARENT":  # casos_propriedade ONLY (classes_or)
-        p[0] = (p[1], p[2], p[4])
-    elif len(p) == 6:  # casos_propriedade SOME CLASSE CARACTERE_ESPECIAL restricoes_axioma_fechamento
-        p[0] = (p[1], p[2], p[3], p[5])
-    elif len(p) == 4:  # casos_propriedade SOME/ONLY CLASSE
-        p[0] = (p[1], p[2], p[3])
-    elif len(p) == 7:  # casos_propriedade COMPARADORES CARDINALIDADE CLASSE CARACTERE_ESPECIAL restricoes_axioma_fechamento
-        p[0] = (p[1], p[2], p[3], p[4], p[6])
-    elif len(p) == 5:  # casos_propriedade COMPARADORES CARDINALIDADE CLASSE
-        p[0] = (p[1], p[2], p[3], p[4])
-    elif len(p) == 4 and p[1] == "ABRE_PARENT":  # (restricoes_axioma_fechamento)
-        p[0] = p[2]
-    elif len(p) == 6 and p[4] == "AND":  # (restricoes_axioma_fechamento) AND restricoes_axioma_fechamento
-        p[0] = (p[2], "AND", p[5])
-    elif len(p) == 6:  # (restricoes_axioma_fechamento) CARACTERE_ESPECIAL restricoes_axioma_fechamento
-        p[0] = (p[2], p[4])
-    else:
-        p[0] = None
 
 def p_casos_propriedade(p):
     """
     casos_propriedade : PROPRIEDADE PROPRIEDADE
                       | PROPRIEDADE
     """
-    if len(p) == 3:  # Dois PROPRIEDADE
-        p[0] = (p[1], p[2])
-    elif len(p) == 2:  # Um PROPRIEDADE
-        p[0] = p[1]
+
 
 def p_classes_or(p):
     """
     classes_or : CLASSE OR classes_or
-              | CLASSE 
+               | CLASSE
+               | ABRE_PARENT classes_or FECHA_PARENT
     """
-    if len(p) == 2:  # Uma única classe
-        p[0] = [p[1]]
-    elif len(p) == 4:  # CLASSE OR outras classes
-        p[0] = [p[1]] + p[3]
+
+
+def p_classes_virgula(p):
+    """
+    classes_virgula : CLASSE CARACTERE_ESPECIAL classes_virgula
+                    | CLASSE
+    """
 
 
 #!======================== CLASSE ENUMERADA ==========================
@@ -408,18 +309,14 @@ def p_declaracao_classe_enumerada(p):
     """
     declaracao_classe_enumerada : ABRE_CHAVE classes_enumeradas FECHA_CHAVE
     """
-    if len(p) == 4:  # { classes_enumeradas }
-        p[0] = p[2]
+    p[0] = (" enumerada ")
 
 def p_classes_enumeradas(p):
     """
     classes_enumeradas : CLASSE CARACTERE_ESPECIAL classes_enumeradas
                        | CLASSE
     """
-    if len(p) == 2:  # Uma única classe
-        p[0] = [p[1]]
-    elif len(p) == 4:  # CLASSE CARACTERE_ESPECIAL outras classes
-        p[0] = [p[1]] + p[3]
+
 
 #!======================== CLASSE COBERTA ============================
 
@@ -427,8 +324,7 @@ def p_declaracao_classe_coberta(p):
     """
     declaracao_classe_coberta : classes_or 
     """
-    if len(p) == 2:  # Apenas classes_or
-        p[0] = p[1]
+    p[0] = (" coberta ")
 
 
 # Erro sintático
@@ -495,7 +391,7 @@ def main():
 
     if opcao == "1":
         try:
-            with open('codigo.txt', 'r') as arquivo:
+            with open('codigo2.txt', 'r') as arquivo:
                 codigo = arquivo.read()
             executar_analisador(codigo)
         except FileNotFoundError:
